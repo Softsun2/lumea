bd753bd24ad3589d230e1a73d5a7aeecc0af315e
softsun2 <peyton.okubo13@gmail.com>
2023-11-19 16:07:23 -0600

    pandoc example

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
-import Text.Pandoc
+
+import qualified Text.Pandoc as P
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  result <- P.runIO $ do
+    doc <- P.readMarkdown P.def (T.pack "[testing](url)")
+    P.writeOrg P.def doc
+  rst <- P.handleError result
+  TIO.putStrLn rst
