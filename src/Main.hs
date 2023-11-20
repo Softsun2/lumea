c69bed866bef8c1432892df256aa04a519deef2b
softsun2 <peyton.okubo13@gmail.com>
2023-11-19 22:51:45 -0600

    add directory traversal util

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,21 @@ module Main where
 import qualified Text.Pandoc as P
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import System.Directory (doesDirectoryExist, listDirectory, getCurrentDirectory)
+import Control.Monad (when)
 
+dirApply :: [FilePath]
+  -> (FilePath -> IO ())
+  -> IO ()
+dirApply [] _ = return ()
+dirApply (fp:fps) f = do
+  f fp
+  isDir <- doesDirectoryExist fp
+  when isDir $ do
+    subFilePaths <- listDirectory fp
+    dirApply subFilePaths f
+  dirApply fps f
+      
 main :: IO ()
 main = do
   result <- P.runIO $ do
@@ -11,3 +25,7 @@ main = do
     P.writeOrg P.def doc
   rst <- P.handleError result
   TIO.putStrLn rst
+
+  cwdFilePath <- getCurrentDirectory
+  cwdFilePaths <- listDirectory cwdFilePath
+  dirApply cwdFilePaths putStrLn
