0cffce692ba19341a0107ef57981fd47e0ca4840
softsun2 <peyton.okubo13@gmail.com>
2023-12-09 21:32:21 -0600

    support concurrent connections

--- src/Server.hs
+++ src/Server.hs
@@ -5,7 +5,8 @@ import Network.Socket.ByteString (recv, sendAll)
 import qualified System.Directory as D
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
-import qualified Control.Concurrent
+import Control.Monad (void)
+import Control.Concurrent (forkFinally)
 
 okHeader :: String
 okHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
@@ -23,9 +24,7 @@ data ReadResult =
 
 readGet :: Socket -> IO ReadResult
 readGet s = do
-  -- Only read 4096 bytes. If this causes a short read the client's
-  -- connection will reset when sending the response.
-  packet <- recv s 4096
+  packet <- recv s 128  -- get header should be <= 128 bytes
   let (getHeader, _) = BS.breakSubstring (BSC.pack "\r\n") packet
   let parsableGetHeader =
         BSC.pack "GET" `BS.isPrefixOf` getHeader &&
@@ -39,25 +38,33 @@ readGet s = do
 parseResource :: String -> FilePath
 parseResource str = tail $ words str !! 1
 
+-- TODO: limit the number of threads to something reasonable,
+-- realisitcally I won't be getting too much traffic
 getOnlyServer :: Socket -> [FilePath] -> IO ()
 getOnlyServer s roots = do
   (c, _) <- accept s
-  getRequest <- readGet c
-  case getRequest of
-    Parsable request -> respondToGet c $ BSC.unpack request
-    ClientDisconnect -> return ()
-    BadRequest -> respondToBadRequest c
-  close c
+  -- It's important to send fin when it's unlikely the server
+  -- reads the entire request.
+  void $ forkFinally (onAccept c) (const $ gracefulClose c 4000)
   getOnlyServer s roots
+
   where
-    respondToGet :: Socket -> String -> IO ()
-    respondToGet c request = do
+    respond :: Socket -> String -> IO ()
+    respond c request = do
       resource <- D.findFile roots (parseResource request)
       case resource of
         Just fp -> do
           contents <- readFile fp
           sendAll c $ BSC.pack okHeader <> BSC.pack contents
         Nothing -> sendAll c $ BSC.pack badReqHeader
+    onAccept :: Socket -> IO ()
+    onAccept c = do
+      getRequest <- readGet c
+      case getRequest of
+        Parsable request -> respond c $ BSC.unpack request
+        ClientDisconnect -> return ()
+        BadRequest -> respondToBadRequest c
+    
 
 run :: IO ()
 run = do
@@ -72,3 +79,4 @@ run = do
   getOnlyServer s ["/Users/softsun2/softsun2/dev/personal/lumea/site/html"]
 
   close s
+  
