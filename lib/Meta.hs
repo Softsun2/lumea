06b09dd1e61810a6ae259f027c8182f038117844
softsun2 <peyton.okubo13@gmail.com>
2024-02-17 15:50:19 -0600

    try custom semantic metadata format

--- lib/Meta.hs
+++ lib/Meta.hs
@@ -2,22 +2,88 @@
 
 module Meta where
 
+import Data.Sequence
 import Data.Text
 import Text.Pandoc
 import Text.Pandoc.Builder
 import Text.Pandoc.Walk
 
--- the wip semantic format of the metadata blocks
--- this isn't really a great idea as the blocks can be used interchangably
--- depending on the markup
+-- Design:
+-- I don't like mixing languages e.g. yaml header with markup body. I don't
+-- like seperating declarations from their references e.g. a meta-lockfile
+-- referenced from source files.
+--
+-- What if we could create a strict semantic structure that is compliant with
+-- whichever source markup format?
+--
+-- This format should:
+--     - format = read $ write format
+--     - be able to encode metadata
+--         - typeless data (strings)
+--         - 1D lists of typeless data (strings)
+--
+-- 1. build metadata
+-- 2. subsitute metadata
+-- 3. convert tail (minus metadata block) to html
+
+type Metadata = [Pair]
+
+type Pair = ([Char], Value) -- string key, value
+
+data Value = Value Char | ValueList [Char] -- string or 1D list of strings
+
+-- with implicit grammar rules
+parseMetadata :: Seq Block -> Maybe Metadata
+parseMetadata blocks = case viewl blocks of
+  Header 3 _ [Str "lumea-metadata"] :< blocks' -> parseBulletList blocks'
+  _ -> Nothing
+
+parseBulletList :: Seq Block -> Maybe Metadata
+parseBulletList blocks = case viewl blocks of
+  BulletList [items] :< _ -> parseListItems $ Data.Sequence.fromList items
+  _ -> Nothing
+
+-- pairs can be key: value or key: list of values
+-- just worry about non-lists for now
+parseListItems :: Seq Block -> Maybe Metadata
+parseListItems items = case viewl items of
+  Plain [key, space, value] :< items' ->
+        -- combine this step's metadata construction
+        -- continue parsing the item sequence
+        -- Maybe Metadata -> Maybe Metadata -> Maybe Metadata
+        -- where: l is Nothing -> nothing
+        --        just l && just r -> Just l cons r
+        --        _ -> Just l
+        Nothing
+  _ -> Nothing
+
+-- grammar rules
+isKey :: Inline -> Bool
+isKey i = case i of
+  Str text -> Data.Text.last text == ':'
+  _ -> False
+
+isValue :: Inline -> Bool
+isValue i = case i of
+  Str _ -> True
+  _ -> False
+
+isLumeaMetadataBlock :: [Block] -> Bool
+isLumeaMetadataBlock bs = case bs of
+  [Header 3 _ [Str "lumea-metadata"], BulletList _] -> True
+  _ -> False
+
+-- subject to change
+-- will this support all the meta data I need?
+-- this is clunky...
+-- what if it wasn't...
 exampleMetadataBlocks :: Blocks
 exampleMetadataBlocks =
-  divWith nullAttr $
-    header 3 (str "lumea-metadata")
-      <> bulletList
-        [ plain $ str "key1" <> space <> str "value1",
-          plain $ str "key2" <> space <> str "value2"
-        ]
+  header 3 (str "lumea-metadata")
+    <> bulletList
+      [ plain $ str "key1" <> space <> str "value1",
+        plain $ str "key2" <> space <> str "value2"
+      ]
 
 -- utility to test for now
 readMarkdownFile :: FilePath -> IO Pandoc
