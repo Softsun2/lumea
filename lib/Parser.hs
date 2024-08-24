2c9145ecf5090c0e0b7b503c02c1e4f39268f807
softsun2 <peyton.okubo13@gmail.com>
2024-08-24 13:14:16 -0500

    implement basic text markup parsing
    still need to parse the markup contents then test

--- lib/Parser.hs
+++ lib/Parser.hs
@@ -2,37 +2,76 @@
 
 module Parser where
 
-import Text.Parsec
-import Text.Parsec.String (Parser)
+import           Control.Monad (void)
+import           Data.Functor (($>))
+import           Text.Parsec
+import           Text.Parsec.String (Parser)
 
 parse' :: Parser a -> String -> a
 parse' parser string = case parse parser "" string of
-  Left e -> error $ show e
+  Left e       -> error $ show e
   Right parsed -> parsed
 
 -- Org syntax reference: https://orgmode.org/worg/org-syntax.html
-
 -- Parsec docs:
 -- https://hackage.haskell.org/package/parsec-3.1.17.0/docs/Text-Parsec.html#t:Parsec
-
 -- Note: Org parse data types are potential candidates for data
 -- herding: https://youtu.be/YR5WdGrpoug?si=vTti5RTBWGZwaUX8
+-- objects:
+-- - plain text
+-- - text markup
+-- - entities
+-- - LaTeX fragments
+-- - super and subscripts
+data Object = TextMarkup [Object]
+            | PlainText String
+  deriving (Show)
 
-data Heading = Heading
-  { stars :: Int,
-    -- keyword :: Maybe String,
-    -- priority :: Maybe Int,
-    -- comment :: Maybe String,
-    title :: Maybe String,
-    tags :: Maybe [String]
-    -- also beleive it has a list of sections
-  }
-  deriving (Show, Eq)
+textMarkup :: Char -> Parser Object
+textMarkup marker = pre *> marker' *> contents <* marker' <* post
+  where
+    marker' = char marker
 
--- todo: confirm Text.Parsec.spaces captures org space characters:
--- spaces, tabs, newlines, and line feeds
-heading :: Parser Heading
-heading = do
-  -- consume stars followed by spaces
-  stars <- (length <$> many1 (char '*')) <* many1 space
-  return $ Heading stars (Just "title") (Just ["tag1", "tag2"])
+    pre = choice
+      [ void $ many1 space
+      , void . choice $ char <$> ['-', '(', '{', '\'', '"']
+      , void $ lookAhead marker']
+
+    -- contents may not begin or end with whitespace
+    contents = manyTill anyChar (lookAhead marker')
+      *> if marker == '=' || marker == '~'
+         then return $ PlainText "verbatim/code"
+         else return $ PlainText "sequence of objects"
+
+    post = eof
+      <|> (choice
+             [ void space
+             , void . choice
+               $ char
+               <$> [ '-'
+                   , '.'
+                   , ','
+                   , ';'
+                   , ':'
+                   , '!'
+                   , '?'
+                   , '\''
+                   , ')'
+                   , '}'
+                   , '['
+                   , '"'
+                   , '\\']]
+           *> eof)
+
+bold = textMarkup '*'
+
+italic = textMarkup '/'
+
+underline = textMarkup '_'
+
+verbatim = textMarkup '='
+
+code = textMarkup '~'
+
+strikethrough = textMarkup '+'
+-- elements
